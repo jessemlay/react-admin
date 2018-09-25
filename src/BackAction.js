8b3b19fcf5b239181d45991210cacfb0ef0c65eb
Jesse <jlay82@gmail.com>
2018-09-25 15:28:36 -0500

    initial commit

---
+++ src/BackAction.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CardActions from '@material-ui/core/CardActions';
+import ApproveButton from './ApproveButton';
+
+const cardActionStyle = {
+    zIndex: 2,
+    display: 'inline-block',
+    float: 'right',
+};
+
+const CommentEditActions = ({ basePath, data, resource }) => (
+    <CardActions style={cardActionStyle}>
+        <ApproveButton record={data} />
+    </CardActions>
+);
+
+export default CommentEditActions;
+
+// in src/comments/index.js
+import CommentEditActions from './CommentEditActions';
+
+export const CommentEdit = (props) =>
+    <Edit {...props} actions={<CommentEditActions />}>
+        ...
+    </Edit>;
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Settings from './containers/Settings';
+
+export default [<Route exact path="/settings" component={Settings} />];
